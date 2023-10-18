c6d1992991698cadd6d9d7ec7aba8a10afc001ed
Administrator <Administrator@MS-KTYEQWUBATIQ>
2023-10-18 16:41:30 +0800

    增加种植

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 
@@ -10,41 +11,106 @@ public partial class Player : CharacterBody2D
     [Export]
     public TileMap MyTileMap;
 
+    [Export]
+    private Timer timer;
+
+
     private Vector2 moveDirection;
 
     private AnimatedSprite2D animatedSprite2D;
     private NavigationAgent2D navigationAgent2D;
 
+
+    int layer = 5;
+    int landLayer = 2;
+    Vector2I atlatsCoors = new Vector2I(6, 2);
+    private Dictionary<Vector2I, int> placeInfo = new Dictionary<Vector2I, int>();
+
     public override void _Ready()
     {
-
         animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
         navigationAgent2D = GetNode<NavigationAgent2D>("NavigationAgent2D");
         navigationAgent2D.SetNavigationMap(MyTileMap.GetNavigationMap(0));
+        navigationAgent2D.VelocityComputed += NavigationAgent2D_VelocityComputed;
+
+        timer.Timeout += HandleGrow;
+        timer.WaitTime = 3;
+        timer.Start();
     }
 
 
+    private void NavigationAgent2D_VelocityComputed(Vector2 safeVelocity)
+    {
+        //GD.Print(safeVelocity);
+    }
+
     public override void _PhysicsProcess(double delta)
     {
-        HandleMouseKey();
-        //HandleInputKey();
+        //HandleMouseKey();
+        HandleInputKey();
+        HandlePlace();
         HandleMove();
         HandleAnimation();
+
+    }
+
+    public void HandleGrow()
+    {
+        foreach (var item in placeInfo)
+        {
+            if (item.Value < 3)
+            {
+                MyTileMap.SetCell(layer, item.Key, 1, new Vector2I(atlatsCoors.X+ item.Value, atlatsCoors.Y));
+                placeInfo[item.Key]++;
+            }
+        }
+    }
+
+    public void HandlePlace()
+    {
+
+        if (Input.IsActionJustPressed("left_mouse"))
+        {
+            Vector2 mousePos = GetGlobalMousePosition();
+            Vector2I mapPos = MyTileMap.LocalToMap(mousePos);
+
+            var tileData = MyTileMap.GetCellTileData(landLayer, mapPos);
+            if (tileData != null)
+            {
+                if (!placeInfo.ContainsKey(mapPos))
+                {
+                    placeInfo.Add(mapPos, 1);
+
+                    Variant canPlaceSeeds = tileData.GetCustomData("can_place");
+                    if (canPlaceSeeds.AsBool())
+                    {
+                        MyTileMap.SetCell(layer, mapPos, 1, atlatsCoors);
+                    }
+                    else
+                    {
+                        GD.Print("can not place");
+                    }
+                }
+                else {
+                    GD.Print("have placed");
+                }
+            }
+        }
     }
 
     public void HandleMouseKey()
     {
         if (Input.IsActionJustPressed("left_mouse"))
         {
-      
+
             navigationAgent2D.TargetPosition = GetGlobalMousePosition();
-            GD.Print(navigationAgent2D.TargetPosition);
+            //GD.Print(navigationAgent2D.TargetPosition);
         }
 
         if (!navigationAgent2D.IsNavigationFinished())
         {
             moveDirection = navigationAgent2D.GetNextPathPosition() - Position;
-            GD.Print(moveDirection);
+            //GD.Print(moveDirection);
         }
         else
         {
@@ -62,8 +128,14 @@ public partial class Player : CharacterBody2D
 
     public void HandleMove()
     {
-        base.Velocity = moveDirection.Normalized() * Speed;
-     
+        var dir = moveDirection.Normalized() * Speed;
+        //GD.Print(dir);
+        //if (!navigationAgent2D.IsTargetReached())
+        //{
+        //    navigationAgent2D.SetVelocityForced(dir);
+        //}
+
+        base.Velocity = dir;
         MoveAndSlide();
     }
 
@@ -101,4 +173,5 @@ public partial class Player : CharacterBody2D
 
 
     }
+
 }
